9fc106891e9ae78e1faf693bf2302f0ee6ba10c6
Tomas R <tomas.ribinskis@gmail.com>
2019-05-26 23:25:39 +0300

    Switched to a shorter video

--- Main.py
+++ Main.py
@@ -14,11 +14,16 @@ total_seconds = 0
 
 def forSeconds(second_number, output_arrays, count_arrays, average_output_count):
     print("SECOND : ", second_number)
+    print(output_arrays)
     global total_seconds
     total_seconds += 1
 
-video_path = detector.detectCustomObjectsFromVideo(custom_objects=custom_objects, input_file_path=(execution_path + "\\molde_short.mp4"),
-                                output_file_path=(execution_path + "\\output\\persons_detected")
-                                , frames_per_second=20, per_second_function=forSeconds, log_progress=True)      #somehow 20fps takes as long to process as 30fps
+video_path = detector.detectCustomObjectsFromVideo(
+    custom_objects=custom_objects, 
+    input_file_path=(execution_path + "\\molde_short_new.mp4"),
+    output_file_path=(execution_path + "\\output\\persons_detected"), 
+    frames_per_second=30, 
+    per_second_function=forSeconds, log_progress=True)
+
 print(video_path)
 print("Total seconds: ", total_seconds)
